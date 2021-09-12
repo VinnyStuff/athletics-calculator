39eb3883b6eba3a8bce2ea38af24076083b0c813
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-09-12 17:10:18 -0300

    conseguindo imprimir um valor do javascript no html na mesma linha

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 function test(){
     document.getElementById("demo").innerHTML = 32;
+
+    document.getElementById("speed").innerHTML = 100;
+
 }
 
 test();
 
 
-function SpeedResult(){
+function Result(){
     if (document.getElementById("distance").value.length == 0){
         alert("Preencha a distância")
+    }
+    else {
+
     }
 }
