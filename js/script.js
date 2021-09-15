8bfda79b16a02b919b31cb646ba08beb9b474091
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-09-15 17:57:29 -0300

    fazendo o esqueleto do que falta

--- js/script.js
+++ js/script.js
@@ -3,14 +3,43 @@ const inputTime = document.getElementById("time");
 const inputPace = document.getElementById("pace");
 const inputDistance = document.getElementById("distance");
 
+function OutputTime(){
+    //get the numbers
+    totalValue = inputDistance.value / inputSpeed.value;
+            
+    hourValue = totalValue;
 
-function GetTheEmptyBox(){
-    const currentValues = [inputSpeed, inputTime, inputPace];
-    for (let i = 0; i < currentValues.length; i++){
-        if (currentValues[i].value > 0){
-            currentValues 
-        }
+    minutesValue = 0.60 * ((totalValue % 1) * 100);
+
+    secondsValue = minutesValue % 1;
+    secondsValue = Math.round(secondsValue * 100);
+    secondsValue = 0.6 * secondsValue;
+
+    if (secondsValue >= 60 ){
+        secondsValue = 0;
+        minutesValue += 1;
     }
+    if (minutesValue >= 60){
+        minutesValue = 0;
+        hourValue += 1;
+    }
+
+    //print
+    document.getElementById("timeHour").value = Math.trunc(hourValue);
+
+    document.getElementById("timeMinutes").value = Math.trunc(minutesValue);
+
+    document.getElementById("timeSeconds").value = Math.trunc(secondsValue);
+
+    //TODO: não arredondar o segundos e colocar decimais // colocar as variaveis que faltam que estão presente no HTML
+}
+
+function OutputPace(){
+
+}
+
+function OutputSpeed(){
+
 }
 
 function Result(){
@@ -18,33 +47,9 @@ function Result(){
         alert("Preencha a distância");
     }
     else{
-        if (Number(inputSpeed.value) > 0){
-            //get the numbers
-            totalValue = inputDistance.value / inputSpeed.value;
-            
-            hourValue = totalValue;
-
-            minutesValue = 0.60 * ((totalValue % 1) * 100);
-
-            secondsValue = minutesValue % 1;
-            secondsValue = Math.round(secondsValue * 100);
-            secondsValue = 0.6 * secondsValue;
-
-            if (secondsValue >= 60 ){
-                secondsValue = 0;
-                minutesValue += 1;
-            }
-            if (minutesValue >= 60){
-                minutesValue = 0;
-                hourValue += 1;
-            }
-
-            //print
-            document.getElementById("timeHour").value = Math.trunc(hourValue);
-
-            document.getElementById("timeMinutes").value = Math.trunc(minutesValue);
-
-            document.getElementById("timeSeconds").value = Math.trunc(secondsValue);
+        if (Number(inputSpeed.value) > 0){ //if have something in speed
+            OutputTime();
+            OutputPace();
         }
     }
 }
