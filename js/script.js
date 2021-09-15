dba04505e83ec5e326f75132d128945657ee97e6
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-09-15 14:37:08 -0300

    quase 100% (quando possui distancia e velocidade)

--- js/script.js
+++ js/script.js
@@ -8,17 +8,26 @@ function GetTheEmptyBox(){
     const currentValues = [inputSpeed, inputTime, inputPace];
     for (let i = 0; i < currentValues.length; i++){
         if (currentValues[i].value > 0){
-            //if ()
-            console.log("bla");
+            currentValues 
         }
     }
 }
 
 function Result(){
-    if (inputDistance.value.length <= 0 || Number(inputDistance.value) == 0){
+    if (Number(inputDistance.value) == 0){
         alert("Preencha a distância");
     }
     else{
-        GetTheEmptyBox();
+        if (Number(inputSpeed.value) > 0){
+            totalValue = inputDistance.value / inputSpeed.value;
+            
+            hourValue = Math.trunc(totalValue);
+            document.getElementById("timeHour").value = hourValue;
+
+            minutesValue = 0.60 * ((totalValue % 1) * 100);
+            document.getElementById("timeMinutes").value = minutesValue;
+
+            console.log(totalValue % 1);
+        }
     }
 }
