7cb46f64c93ae917d586315ba3eb34cafc53b7e7
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-09-20 01:20:27 -0300

    deixando o script mais facil de ler

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ const inputTimeSeconds = document.getElementById("timeSeconds");
 const inputPaceMinutes = document.getElementById("paceMinutes");
 const inputPaceSeconds = document.getElementById("paceSeconds");
 
-const allInputs = [inputSpeed, inputDistance, inputTimeHour,  inputTimeMinutes, inputTimeSeconds, inputPaceMinutes, inputPaceSeconds]
+const allInputs = [inputSpeed, inputDistance, inputTimeHour, inputTimeMinutes, inputTimeSeconds, inputPaceMinutes, inputPaceSeconds]
 
-function OutputTime(){
+function Time() {
     //get the numbers
     let totalValue = inputDistance.value / inputSpeed.value;
-            
+
     let hourValue = totalValue;
 
     let minutesValue = 0.60 * ((totalValue % 1) * 100);
@@ -23,11 +23,11 @@ function OutputTime(){
     secondsValue = Math.round(secondsValue * 100);
     secondsValue = 0.6 * secondsValue;
 
-    if (secondsValue >= 60 ){
+    if (secondsValue >= 60) {
         secondsValue = 0;
         minutesValue += 1;
     }
-    if (minutesValue >= 60){
+    if (minutesValue >= 60) {
         minutesValue = 0;
         hourValue += 1;
     }
@@ -40,8 +40,8 @@ function OutputTime(){
     //TODO: não arredondar o segundos e colocar decimais // colocar as variaveis que faltam que estão presente no HTML
 }
 
-function OutputPace(whereFrom){
-    if ("Speed"){
+function Pace(whereFrom) {
+    if (whereFrom == "Speed") {
         let paceValue = 60 / (Number(inputSpeed.value))
 
         let paceMinutesValue = Math.trunc(paceValue);
@@ -54,41 +54,41 @@ function OutputPace(whereFrom){
         inputPaceMinutes.value = paceMinutesValue;
         inputPaceSeconds.value = paceSecondsValue;
     }
-    else{
+    else {
         let hourValue = inputTimeHour.value * 60;
 
         let minutesValue = Number(inputTimeMinutes.value) + hourValue;
-    
+
         let totalPaceValue = minutesValue / inputDistance.value;
-        
+
         let PaceMinValue = Math.trunc(totalPaceValue);
-    
+
         let PaceSecondsValue = totalPaceValue % 1;
         PaceSecondsValue = Math.round(PaceSecondsValue * 100);
         PaceSecondsValue = 0.6 * PaceSecondsValue;
-    
+
         inputPaceMinutes.value = PaceMinValue;
         inputPaceSeconds.value = PaceSecondsValue;
     }
 }
 
-function OutputSpeed(whereFrom){
-    if (whereFrom == "Pace"){ //only have pace
+function Speed(whereFrom) {
+    if (whereFrom == "Pace") { //only have pace
         let speedValue = 60 / ((Number(inputPaceSeconds.value) / 60) + (Number(inputPaceMinutes.value)));
         console.log(speedValue);
         inputSpeed.value = speedValue.toFixed(2);
     }
-    else{
+    else {
         let timeValue = (Number(inputTimeSeconds.value) / 60) + (Number(inputTimeMinutes.value) / 60) + Number(inputTimeHour.value);
         let distanceValue = Number(inputDistance.value);
-    
+
         let speedValue = distanceValue / timeValue;
         inputSpeed.value = speedValue.toFixed(2);
     }
 }
 
-function OutputDistance(whereFrom){
-    if (whereFrom == "Time"){
+function Distance(whereFrom) {
+    if (whereFrom == "Time") {
         //time
         let timeValue = (Number(inputTimeHour.value) * 60) + (Number(inputTimeMinutes.value)) + (Number(inputTimeSeconds.value) / 60);
 
@@ -98,57 +98,93 @@ function OutputDistance(whereFrom){
 
         inputDistance.value = distanceValue;
     }
-    else{
+    else {
         let distanceValue = Number(inputSpeed.value) * (((Number(inputTimeSeconds.value / 60) + Number(inputTimeMinutes.value)) / 60) + Number(inputTimeHour.value));
 
         inputDistance.value = distanceValue.toFixed(2);
     }
 }
-
-function Calculate(){
-    //PutZero();
-    if (InputTimeHaveSomething() == true){//if have something in time
-        OutputSpeed(); //need distance 
-        //OutputPace(); //need the speed
-        //OutputDistance("Time") //need the time(need transform everything in minutes), pace 
-    }
-    else if (Number(inputSpeed.value) > 0){ //if have something in speed
-        if (Number(inputTimeHour.value) > 0 || Number(inputTimeMinutes.value) > 0 || Number(inputTimeSeconds.value) > 0){
-            OutputDistance();
-            OutputPace();
-        }
-        else{
-            OutputPace("Speed");
-            OutputTime();
-        }
-    }
-    else if (Number(inputDistance.value) > 0){ //if have something in distance
-
-    }
-    else if (Number(inputPaceMinutes.value) > 0 || Number(inputPaceSeconds.value) > 0){ //if something in pace
-        OutputSpeed("Pace");
-    }
-    //TODO: CALCULADORA FUNCIONANDO, AGORA PEGAR O VALOR QUE MUDOU E REALIZAR OS OUTROS CALCULOS EM CIMA DELE
-}
-
-function InputTimeHaveSomething(){
-    if (Number(inputTimeHour.value) > 0 || Number(inputTimeMinutes.value) > 0  || Number(inputTimeSeconds.value) > 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-function Clear(){
-    for (let i = 0; i < allInputs.length; i++){
+function Clear() {
+    for (let i = 0; i < allInputs.length; i++) {
         allInputs[i].value = "";
     }
 }
 
-function PutZero(){ //if the input is null, put zero
-    for (let i = 0; i < allInputs.length; i++){
-        if (allInputs[i].value == ""){
+function PutZero() { //if the input is null, put zero
+    for (let i = 0; i < allInputs.length; i++) {
+        if (allInputs[i].value == "") {
             allInputs[i].value = "0";
         }
     }
+}
+
+function InputTimeHaveSomething() {
+    if (Number(inputTimeHour.value) > 0 || Number(inputTimeMinutes.value) > 0 || Number(inputTimeSeconds.value) > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function InputSpeedHaveSomething() {
+    if (Number(inputSpeed.value) > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function InputDistaceHaveSomething() {
+    if (Number(inputDistance.value) > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function InputPaceHaveSomething() {
+    if (Number(inputPaceMinutes.value) > 0 || Number(inputPaceSeconds.value) > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function Calculate() {
+    if (InputTimeHaveSomething() == true) {
+        if (InputDistaceHaveSomething() == true) {
+            Speed();
+            Pace("Speed");
+        }
+        else if (InputPaceHaveSomething() == true) {
+            Speed("Pace");
+            Distance();
+        }
+        else if (InputSpeedHaveSomething() == true) {
+            Pace("Speed");
+            Distance();
+        }
+    }
+    else if (InputSpeedHaveSomething() == true) {
+        if (InputTimeHaveSomething() == true) { //pode apagar
+            Distance();
+            Pace();
+        }
+        else {
+            Pace("Speed");
+            Time();
+        }
+    }
+    else if (InputDistaceHaveSomething() == true) {
+
+    }
+    else if (InputPaceHaveSomething() == true) {
+        Speed("Pace");
+    }
+    //TODO: CALCULADORA FUNCIONANDO, AGORA PEGAR O VALOR QUE MUDOU E REALIZAR OS OUTROS CALCULOS EM CIMA DELE
 }
